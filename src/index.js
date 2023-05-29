899235d76f12939e12ea47f849f4238d0fb36a79
florence <florencemaffini@hotmail.fr>
2023-05-29 21:29:33 +0200

    29/05/23
    création du components error

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Error from './components/Error'
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/pages/home" element={<Home />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </Router>
     <Footer />
