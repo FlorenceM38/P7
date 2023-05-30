86e719248b6f3f983aa177b52467b53b0a3543a6
florence <florencemaffini@hotmail.fr>
2023-05-30 21:48:38 +0200

    30/05/23

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import '../styles/card.css'
 
-function Card() {
-  return <div></div>
+function Card({ title, id, cover }) {
+  return (
+    <Link to={`/logements/${id}`} className="card">
+      <img src={cover} alt={title} className="coverCard" />
+      <h3 className="h3Card">{title}</h3>
+    </Link>
+  )
 }
 
 export default Card
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import Banner from '../components/Banner'
 import Card from '../components/Card'
+import { logements } from '../datas/logements.json'
 
 function Home() {
   return (
     <section>
       <Banner />
-      <Card />
+      <div>
+        {logements.map((logements) => {
+          return (
+            <Card
+              key={logements.id}
+              id={logements.id}
+              title={logements.title}
+              cover={logements.cover}
+            />
+          )
+        })}
+      </div>
     </section>
   )
 }
