ea627106d1f1e7829399d084684f41c5822419d7
florence <florencemaffini@hotmail.fr>
2023-05-31 20:30:52 +0200

    31/05/23
    css de la page home en version pc

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -8,7 +8,7 @@ function Home() {
   return (
     <section>
       <Banner />
-      <div>
+      <div className="home">
         {Logements.map((logements) => {
           return (
             <Card
